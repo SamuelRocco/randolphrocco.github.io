f90323959c64ff98df614e6ccff7b991d6affa77
Samuel Rocco <samueljrocco@gmail.com>
2025-04-11 20:05:31 -0400

    v2

--- 01-pages/Health Insurance Term Glossary Interactive Guide/story_content/triggers.js
+++ 01-pages/Health Insurance Term Glossary Interactive Guide/story_content/triggers.js
@@ -2,8 +2,203 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6VtOlW2DyeA":
+      case "5zsoz99dfZ2":
         Script1();
+        break;
+      case "6bjLJPutsEC":
+        Script2();
+        break;
+      case "6gfzbnPQXwK":
+        Script3();
+        break;
+      case "67KdnhbVUR1":
+        Script4();
+        break;
+      case "5ZpmQrxHp9Q":
+        Script5();
+        break;
+      case "6ekuDn2z3gX":
+        Script6();
+        break;
+      case "5Wo3jImgihD":
+        Script7();
+        break;
+      case "6lMBTMeTviq":
+        Script8();
+        break;
+      case "5xSKsueOdHO":
+        Script9();
+        break;
+      case "5iptxxUvVsE":
+        Script10();
+        break;
+      case "5dZM1Us9npU":
+        Script11();
+        break;
+      case "5vnURFgojQk":
+        Script12();
+        break;
+      case "69ZyODOp0M4":
+        Script13();
+        break;
+      case "69rUULiKT14":
+        Script14();
+        break;
+      case "6LbZWNuZRTc":
+        Script15();
+        break;
+      case "5zxiRibahzt":
+        Script16();
+        break;
+      case "6ijZZPLwCgM":
+        Script17();
+        break;
+      case "6lOLhGoT1vo":
+        Script18();
+        break;
+      case "5zLrsK6PGXQ":
+        Script19();
+        break;
+      case "6angBkScEEt":
+        Script20();
+        break;
+      case "6T1KVyJpBYP":
+        Script21();
+        break;
+      case "5VdXS6wKye1":
+        Script22();
+        break;
+      case "5qft1TQvUxi":
+        Script23();
+        break;
+      case "67IYbFvGcK4":
+        Script24();
+        break;
+      case "601CoBLqgKP":
+        Script25();
+        break;
+      case "6csY2LIUfVO":
+        Script26();
+        break;
+      case "6KhNrSOtCK9":
+        Script27();
+        break;
+      case "5gVp8e5kwE9":
+        Script28();
+        break;
+      case "6AwLqOzJFxn":
+        Script29();
+        break;
+      case "5o4G0MoHFOz":
+        Script30();
+        break;
+      case "6RmcduGVC7d":
+        Script31();
+        break;
+      case "5gzB9FfqFQo":
+        Script32();
+        break;
+      case "5WphW5VLfQv":
+        Script33();
+        break;
+      case "5wNRljf0sXh":
+        Script34();
+        break;
+      case "6BEaF6dPhxR":
+        Script35();
+        break;
+      case "6oj0NrWPziu":
+        Script36();
+        break;
+      case "6Mut7T2JpQM":
+        Script37();
+        break;
+      case "5tVOepch1Nc":
+        Script38();
+        break;
+      case "5dJ4mgo7r1O":
+        Script39();
+        break;
+      case "6dQ8yPFgoZt":
+        Script40();
+        break;
+      case "6C9BnM78IEa":
+        Script41();
+        break;
+      case "5o6DURFoiOo":
+        Script42();
+        break;
+      case "6QphxWlHUMe":
+        Script43();
+        break;
+      case "5fUbM387JB6":
+        Script44();
+        break;
+      case "6V92Xmt9fKp":
+        Script45();
+        break;
+      case "5dxowN2Xvoo":
+        Script46();
+        break;
+      case "5cFRnTy2Dgz":
+        Script47();
+        break;
+      case "6r3w5TQt6JU":
+        Script48();
+        break;
+      case "5Xb0pF4IRCM":
+        Script49();
+        break;
+      case "6kXKESf46BC":
+        Script50();
+        break;
+      case "6ZRHkWhzy1T":
+        Script51();
+        break;
+      case "5hIXF5tBC2f":
+        Script52();
+        break;
+      case "6jZIC63FaNE":
+        Script53();
+        break;
+      case "6hOqx6zDznT":
+        Script54();
+        break;
+      case "6YBVm1vm7uI":
+        Script55();
+        break;
+      case "5v87hUyrtQ2":
+        Script56();
+        break;
+      case "6g9kZlJezQO":
+        Script57();
+        break;
+      case "6quzLOTAI2J":
+        Script58();
+        break;
+      case "61ZGzTOSRTs":
+        Script59();
+        break;
+      case "5iFzSTnHUvs":
+        Script60();
+        break;
+      case "6O92yLKrEDT":
+        Script61();
+        break;
+      case "5Yz6dstMTJB":
+        Script62();
+        break;
+      case "6KN9Qw3Qi92":
+        Script63();
+        break;
+      case "60Rd73ofv2t":
+        Script64();
+        break;
+      case "6a0xCAkCokD":
+        Script65();
+        break;
+      case "5YPkGq6AT3i":
+        Script66();
         break;
   }
 }
@@ -31,7 +226,7 @@ const duration = 750;
 const easing = 'ease-out';
 const id = '5ixeQHIGPdh';
 const pulseAmount = 0.07;
-const delay = 0;
+const delay = 250;
 addToTimeline(
 target.animate([
 { scale: '1' }, { scale: `${1 + pulseAmount}` },
--- 01-pages/Health Insurance Term Glossary Interactive Guide/story_content/user.js
+++ 01-pages/Health Insurance Term Glossary Interactive Guide/story_content/user.js
@@ -13,4 +13,329 @@ var showPointer = player.showPointer;
 var hidePointer = player.hidePointer;
 var slideWidth = player.slideWidth;
 var slideHeight = player.slideHeight;
+window.Script2 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script3 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script4 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script5 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script6 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script7 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script8 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script9 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script10 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script11 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script12 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script13 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script14 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script15 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script16 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script17 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script18 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script19 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script20 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script21 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script22 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script23 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script24 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script25 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script26 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script27 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script28 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script29 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script30 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script31 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script32 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script33 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script34 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script35 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script36 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script37 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script38 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script39 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script40 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script41 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script42 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script43 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script44 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script45 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script46 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script47 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script48 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script49 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script50 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script51 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script52 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script53 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script54 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script55 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script56 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script57 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script58 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script59 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script60 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script61 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script62 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script63 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script64 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script65 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
+window.Script66 = function()
+{
+  window.location.href = "../../01-pages/subsite/index.html"
+}
+
 };
